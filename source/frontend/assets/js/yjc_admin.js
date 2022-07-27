b74470ee4fa1671dff68732561867631ae55a6f9
DavidLlanio <llaniodavid@gmail.com>
2022-07-26 23:36:26 -0400

    FEATURE: admin can send settings to ws server

---
+++ source/frontend/assets/js/yjc_admin.js
@@ -0,0 +1,42 @@
+// Connect to websocket server
+const ws_admin = new WebSocket("ws://localhost:8000/admin/ws");
+
+// handle heartbeat
+ws_admin.onmessage = function(event){
+    const msg = JSON.parse(event.data);
+    
+    if (msg.eventcode == 0){
+        ws_admin.send("1");
+    }
+}
+
+function sendSettings(event){
+    // Get toggle switch states
+    const tog1s = document.getElementById("tog1").checked;
+    const tog2s = document.getElementById("tog2").checked;
+    const tog3s = document.getElementById("tog3").checked;
+    const tog4s = document.getElementById("tog4").checked;
+    const tog5s = document.getElementById("tog5").checked;
+    
+    // Create packet
+    const msg = {
+        auth: "admin",
+        features: {
+            ai: tog1s,
+            uep: tog2s,
+            upep: tog3s,
+            as: tog4s,
+            de: tog5s
+        },
+        content: ""
+    };
+    
+    // Send packet
+    ws_admin.send(JSON.stringify(msg));
+    
+}
+
+function backOut(event){
+    ws_admin.close();
+    window.location.href = "localhost:8000/";
+}
