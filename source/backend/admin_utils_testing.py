33a14730f5f143ee3e516694677f55a154e32052
TheAntiSnipe <38178591+TheAntiSnipe@users.noreply.github.com>
2022-07-27 10:42:13 -0400

    Added instructions to run admin_utils_testing.yaml

--- source/backend/admin_utils_testing.py
+++ source/backend/admin_utils_testing.py
@@ -1,5 +1,8 @@
 import admin_utils
 
+# Note: To run these tests,
+# docker compose -f "docker-compose.admintesting.yaml" up --build
+
 settings = admin_utils.Settings()
 print("Current table:")
 print(settings.return_channel_document())
