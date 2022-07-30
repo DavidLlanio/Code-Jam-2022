e81fb847f248798965b8f0c35eb15a8dff20c3cc
chrisgower <chris.gower@deloaches.com>
2022-07-30 12:50:11 -0500

    FEATURE: initial admin move to gateway

--- source/backend/gateway/gateway.py
+++ source/backend/gateway/gateway.py
@@ -1,10 +1,15 @@
 import asyncio
 import json
+import os
 from typing import ClassVar
 
 import websockets
 
+from ..admin_utils import Settings
+
 __all__: list[str] = ["Gateway"]
+settings = Settings()
+admin_password = os.getenv("USERNAME", "secret")
 
 
 class Gateway:
@@ -41,8 +46,54 @@ class Gateway:
                 case 0:
                     await websocket.ping("{eventcode: 0, data: {}}")
                 case 8:
-                    # change the admin settings
-                    ...
+                    # TODO: figure out how to do admin password checking in gateway
+                    # while input_password != admin_password:
+                    #     data = await websocket.receive_json()
+                    #     input_password = data.get("password", None)
+                    #     if input_password == admin_password:
+                    #         await websocket.send_json(
+                    #             {
+                    #                 "message": "correct password",
+                    #                 "features": settings.return_frontend_settings(),
+                    #             }
+                    #         )
+                    #         # admin_client_list.append(websocket)
+                    #         break
+                    #     elif input_password is None:
+                    #         await websocket.send_json({"message": "missing password"})
+                    #     else:
+                    #         await websocket.send_json({"message": "incorrect password"})
+
+                    # monitors websocket for changes to the settings
+                    data = await websocket.receive_json()
+
+                    # maps the settings from the received frontend shorthand names to the longform backend names
+                    received_settings = data.get("features", None)
+                    settings_remap = {
+                        "ai": "randomize_username",
+                        "allow_edit_messages": "uep",
+                        "allow_edit_avatars": "upep",
+                        "sort_by_alpha": "as",
+                        "double_english": "de",
+                    }
+                    mapped_settings = {
+                        settings_remap[k]: received_settings[k]
+                        for k in received_settings
+                    }
+
+                    if mapped_settings:
+                        settings.update_settings(mapped_settings)
+
+                    # TODO: notify chat clients when settings have changed
+
+                    # sends an updated list of settings to all admin clients
+                    # TODO: send out to all admin websocket connections
+                    # for admin_client in admin_client_list:
+                    #     if admin_client != websocket:
+                    #         await websocket.send_json(
+                    #             {"features": settings.return_frontend_settings()}
+                    #         )
+
                 case other:
                     pass
 
