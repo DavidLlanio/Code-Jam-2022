78fa0e471ff796b3545f5c434171cdb9745ecaef
chrisgower <chris.gower@deloaches.com>
2022-07-30 10:55:27 -0500

    FEATURE:Admin backend renames settings to frontend

--- source/backend/admin_utils.py
+++ source/backend/admin_utils.py
@@ -28,6 +28,20 @@ class Settings(object):
         """Returns the current admin_table."""
         return self.admin_table
 
+    def return_frontend_settings(self) -> dict[str, bool]:
+        """Retrieves the settings and remaps them to the frontend short names"""
+        settings = self.admin_table
+        settings_remap = {
+            "randomize_username": "ai",
+            "uep": "allow_edit_messages",
+            "upep": "allow_edit_avatars",
+            "as": "sort_by_alpha",
+            "de": "double_english",
+        }
+        frontend_mapped_settings = {settings_remap[k]: settings[k] for k in settings}
+
+        return frontend_mapped_settings
+
     def update_settings(self, settings_to_update: dict[str, bool]) -> None:
         """Edits the required properties"""
         self.admin_table = self.admin.change_properties(settings_to_update)
